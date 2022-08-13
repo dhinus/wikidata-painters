50511d91157a299bc4abdd26f2cd7a0f275a1969
Francesco Negri <fnegri@wikimedia.org>
2022-08-13 21:34:19 +0200

    Use faster query suggested by lucaswerkmeister

--- src/lib/wikidata.js
+++ src/lib/wikidata.js
@@ -13,15 +13,21 @@ class SPARQLQueryDispatcher {
 }
 
 const endpointUrl = 'https://query.wikidata.org/sparql';
-const paintersQuery = `SELECT ?painterLabel ?painting ?paintingLabel ?image (MD5(CONCAT(str(?painting),str(RAND()))) as ?random) WHERE {
-  ?painting wdt:P18 ?image .
-  ?painting wdt:P31 wd:Q3305213 . # instance of (P31) painting (Q3305213)
-  ?painting wdt:P170 ?painter .   # created by (P170)
-  ?painter wdt:P106 wd:Q1028181 . # give me all people with occupation (P106) painter (Q1028181)
-  ?painter wdt:P135 wd:Q40415 .   # who belonged to the impressionist (Q40415) movement (P135)
-  SERVICE wikibase:label {bd:serviceParam wikibase:language "en"}
-} ORDER BY ?random
-LIMIT 5`;
+const paintersQuery = `SELECT ?painterLabel ?painting ?paintingLabel ?image WITH {
+  SELECT ?painter ?painting ?image WHERE {
+    ?painting wdt:P18 ?image .
+    ?painting wdt:P31 wd:Q3305213 . # instance of (P31) painting (Q3305213)
+    ?painting wdt:P170 ?painter .   # created by (P170)
+    ?painter wdt:P106 wd:Q1028181 . # give me all people with occupation (P106) painter (Q1028181)
+    ?painter wdt:P135 wd:Q40415 .   # who belonged to the impressionist (Q40415) movement (P135)
+    BIND(MD5(CONCAT(str(?painting),str(RAND()))) as ?random)
+  }
+  ORDER BY ?random
+  LIMIT 5
+} AS %results WHERE {
+  INCLUDE %results.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}`;
 
 const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
 
